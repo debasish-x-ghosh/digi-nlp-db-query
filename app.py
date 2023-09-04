ac950c7473f8fc52b1aa57943d99969ad184344d
techguru <profile.dghosh@gmail.com>
2023-09-03 20:27:27 -0400

    6th commit

--- app.py
+++ app.py
@@ -1,86 +1,31 @@
-import torch
-torch.__version__
-
-import streamlit as st
-
-from transformers import pipeline
+from transformers import AutoModelForTableQuestionAnswering, AutoTokenizer, pipeline
 import pandas as pd
-import os
 
-#App UI starts here 
-st.header("Chatbot using LLM")
+data = pd.read_csv(r"data.csv")
+print(data)
+data = data.astype(str)
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+# Load model & tokenizer
+model = 'google/tapas-base-finetuned-wtq'
+tapas_model = AutoModelForTableQuestionAnswering.from_pretrained(model)
+tapas_tokenizer = AutoTokenizer.from_pretrained(model)
 
-# Tabs
-tab1, tab2 = st.tabs(["NLP-DataQuery", "Project2"])
-
-with tab1:  
-        st.header('NLP-DataQuery')
-        st.subheader('Drill down all options')
-        
-with tab2:
-        st.header('Project2')
-        st.subheader('Drill down all options')
-
-#!! TAB1 starts
-#Gets the user input
-if(pipeline != None):
-    tqa = pipeline(task="table-question-answering", 
-                    model="google/tapas-base-finetuned-wtq")
-
-    table = pd.read_csv("data.csv")
-    table = table.astype(str)
-    tab1.dataframe(table)
-    print(table)
+# Initializing pipeline
+nlp = pipeline('table-question-answering', model=tapas_model, tokenizer=tapas_tokenizer)
 
 
-def load_answer(question):
-    print(question)
-    #!!-Mutiple Question starts
-    query = question.split(',')
-    print("query: ")
-    print(len(query))
-    finalanswer = ""
-    print("-----------------------answer----------------------")
-    
-    if(len(query) == 1):
-        #!!-Single Question starts 
-        finalanswer = tqa(table=table, query=query)["answer"] 
-        return finalanswer
-        #!!-Single Question ends
-    else:
-        #!!-Mutiple Question starts
-        answer = tqa(table=table, query=query)
-        for ans in answer:
-            print("*****************")
-            print(ans["answer"])
-            finalanswer += "ANSWER: " + ans["answer"]  + ', '
-        return finalanswer
-        #!!-Mutiple Question ends
-    
+def qa(query,data):
+    print('>>>>>')
+    print(query)
+    result = nlp({'table': data,'query':query})
+    answer = result['cells']
+    print(answer)
 
-#!! TAB1 starts
-#Gets the user input
-def get_text():
-    input_text = tab1.text_input("Please ask: (PS: add multiple questions with ',' separation)", key="input")
-    return input_text
+prediction = qa('Which sri lankan batsman has average below 35',data)
+print(prediction)
 
-user_input = get_text()
-if(user_input != ""):
-    response = load_answer(user_input)
 
-submit = tab1.button('get answer')  
+prediction = qa('who is the third highest scorer?',data)
+print(prediction)
 
-#If generate button is clicked
-if submit:
-    tab1.subheader("Answer:")
-    tab1.write(response)
 
-#!! TAB1 ends
